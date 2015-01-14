fff4b150c6edd44c1c115174886c4b8fe4021638
Torrie Fischer <torrie@ripple.com>
2015-01-14 14:04:38 -0800

    Handle events in executors tests

--- tests/executors.rs
+++ tests/executors.rs
@@ -4,12 +4,39 @@ extern crate "posix-ipc" as ipc;
 use ipc::signals;
 
 #[test]
+fn exec_bin_false() {
+    let argv = ["/usr/bin/false"];
+    let exec = sandbox::executors::Execv::new(&argv);
+    let mut sbox = sandbox::Sandbox::new(Box::new(exec));
+    sbox.spawn();
+    assert!(sbox.get_pid() != -1);
+    loop {
+        match sbox.tick() {
+            sandbox::events::Event::Exit(st) => {
+                assert!(st == 1);
+                break;
+            },
+            _ => {}
+        }
+    }
+}
+
+#[test]
 fn exec_bin_true() {
     let argv = ["/usr/bin/true"];
     let exec = sandbox::executors::Execv::new(&argv);
     let mut sbox = sandbox::Sandbox::new(Box::new(exec));
     sbox.spawn();
     assert!(sbox.get_pid() != -1);
+    loop {
+        match sbox.tick() {
+            sandbox::events::Event::Exit(st) => {
+                assert!(st == 0);
+                break;
+            },
+            _ => {}
+        }
+    }
 }
 
 #[test]
@@ -18,6 +45,32 @@ fn exec_closure() {
     let mut sbox = sandbox::Sandbox::new(Box::new(exec));
     sbox.spawn();
     assert!(sbox.get_pid() != -1);
+    loop {
+        match sbox.tick() {
+            sandbox::events::Event::Exit(st) => {
+                assert!(st == 0);
+                break;
+            },
+            _ => {}
+        }
+    }
+}
+
+#[test]
+fn exec_closure_with_return() {
+    let exec = sandbox::executors::Function::new(Box::new(move |&:| -> i32 {42}));
+    let mut sbox = sandbox::Sandbox::new(Box::new(exec));
+    sbox.spawn();
+    assert!(sbox.get_pid() != -1);
+    loop {
+        match sbox.tick() {
+            sandbox::events::Event::Exit(st) => {
+                assert!(st == 42);
+                break;
+            },
+            _ => {}
+        }
+    }
 }
 
 #[test]
